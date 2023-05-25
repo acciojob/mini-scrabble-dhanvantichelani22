7f852ab64e845104a1edd4caf103b6e05f8c64bb
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-05-25 23:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function  textCount(event) {
+   let letterCnt = document.getElementById("letterCount");
+  // let letterCount = 0;
+
+const text = event.target.value;
+	const len = text.length;
+	if(text === ""){
+		document.getElementById("letterCount").innerText = 0;	
+	}else{
+		document.getElementById("letterCount").innerText = len;
+	}
+	
+}
+const txt = document.getElementById("evaluatedText");
+txt.addEventListener("click", ()=>{
+	document.getElementById("letterCount").innerText = 0;
+})
+txt.addEventListener("txt", (event) => {
+	textCount(event);
+});
